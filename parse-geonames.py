95a11ca86fe066a5808b9413c4a8ab62e854956f
David Megginson <david.megginson@gmail.com>
2019-09-03 16:09:30 -0400

    Start modularising code.

--- parse-geonames.py
+++ parse-geonames.py
@@ -1,3 +1,15 @@
+""" Compile a name map from GeoNames data
+
+Usage:
+
+    python parse_geonames.py allCountries.txt > name-map.lines.json
+
+The geocode.py module uses the output.
+
+Started 2019-09 by David Megginson
+This code is in the public domain
+"""
+
 import fileinput, json, logging, re, sys
 import iso3
 
@@ -26,14 +38,6 @@ keys = (
     "modification_date",
 )
 
-WS_PATTERN = re.compile('\W+')
-
-def normalise (s):
-    """ Generate a normalised version of a string """
-    return WS_PATTERN.sub(' ', s).lower().strip()
-
-mapping_table = dict()
-country_codes_seen = set()
 
 FEATURE_WEIGHTS = {
     # countries and country-like things
@@ -49,38 +53,53 @@ FEATURE_WEIGHTS = {
     'PPLC': 2,
     'PPLA': 2,
 }
-""" Feature names that get extra weight """
+""" GeoNames feature names that get extra weight """
 
-for row in fileinput.input():
-    record = dict(zip(keys, row.split("\t")))
-    country_code = iso3.MAP.get(record['country_code'])
-    if country_code is not None:
-        alternate_names = record['alternatenames'].split(",")
-        names = [record['name']] + alternate_names
-        if record['feature_class'] in ('A', 'P',):
-            if country_code not in country_codes_seen:
-                logger.info("Processing %s", country_code)
-                country_codes_seen.add(country_code)
-            for name in names:
-                if name:
-                    name = normalise(name)
 
-                    # create the map of country codes, if it doesn't already exist
-                    if not mapping_table.get(name):
-                        mapping_table[name] = dict()
+WS_PATTERN = re.compile('\W+')
 
-                    # raise to the appropriate weight for the feature type
-                    weight = FEATURE_WEIGHTS.get(record['feature_code'], 1)
-                    if len(alternate_names) > 5:
-                        weight += 1 # prominance bonus for many translations
-                    current_weight = mapping_table[name].get(country_code)
-                    if current_weight is None or current_weight < weight:
-                        mapping_table[name][country_code] = weight
+def normalise (s):
+    """ Generate a normalised version of a string """
+    return WS_PATTERN.sub(' ', s).lower().strip()
 
-entries_written = 0
-for entry in mapping_table.items():
-    json.dump(list(entry), sys.stdout)
-    print('')
-    entries_written += 1
-    if (entries_written % 1000000) == 0:
-        logger.info("Wrote %d entries", entries_written)
+def parse_geonames (input, output):
+    mapping_table = dict()
+    country_codes_seen = set()
+
+    for row in input:
+        record = dict(zip(keys, row.split("\t")))
+        country_code = iso3.MAP.get(record['country_code'])
+        if country_code is not None:
+            alternate_names = record['alternatenames'].split(",")
+            names = [record['name']] + alternate_names
+            if record['feature_class'] in ('A', 'P',):
+                if country_code not in country_codes_seen:
+                    logger.info("Processing %s", country_code)
+                    country_codes_seen.add(country_code)
+                for name in names:
+                    if name:
+                        name = normalise(name)
+
+                        # create the map of country codes, if it doesn't already exist
+                        if not mapping_table.get(name):
+                            mapping_table[name] = dict()
+
+                        # raise to the appropriate weight for the feature type
+                        weight = FEATURE_WEIGHTS.get(record['feature_code'], 1)
+                        if len(alternate_names) > 5:
+                            weight += 1 # prominance bonus for many translations
+                        current_weight = mapping_table[name].get(country_code)
+                        if current_weight is None or current_weight < weight:
+                            mapping_table[name][country_code] = weight
+
+    entries_written = 0
+    for entry in mapping_table.items():
+        json.dump(list(entry), output)
+        print('', file=output)
+        entries_written += 1
+        if (entries_written % 1000000) == 0:
+            logger.info("Wrote %d entries", entries_written)
+
+
+if __name__ == "__main__":
+    parse_geonames(fileinput.input(), sys.stdout)
